e5b07ad9793ae6f30d09a73c97dc4a48a7919d1c
Juan Navarro <juani.navarrog@gmail.com>
2015-08-07 13:12:17 -0500

    Removed ribbon.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,8 +4,8 @@ $(document).ready(function() {
 		padding : 0,
 		autoResize: true,
 		beforeShow: function () {
-			this.title = $(this.element).attr('title');
-			this.title = '<h4>' + this.title + '</h4>' + '<p>' + $(this.element).parent().find('img').attr('alt') + '</p>';
+			this.title = '<h4>' + $(this.element).attr('title') + '</h4>';
+			//this.title = '<h4>' + this.title + '</h4>' + '<p>' + $(this.element).parent().find('img').attr('alt') + '</p>';
 		},
 		helpers : {
 			title : { type: 'inside' },
